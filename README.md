0b770a1d2f5de970cdab749de2c764f839a9b679
Yu-han Lin <guesslin1986@gmail.com>
2015-07-30 18:13:19 +0800

    Init Commit for dice

---
+++ README.md
@@ -0,0 +1,8 @@
+DICE
+====
+
+dice for assign number of dice
+
+	dice 2d6
+
+roll two dice in six sides
---
+++ dice.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func main() {
+	var num, dice int32
+	var result = make([]int32, 0)
+	rand.Seed(time.Now().UTC().UnixNano())
+	if len(os.Args) < 2 {
+		num = 1
+		dice = 6
+	} else {
+		args := os.Args[1]
+		fmt.Sscanf(args, "%dd%d", &num, &dice)
+	}
+	for i := int32(0); i < num; i++ {
+		result = append(result, rand.Int31n(dice))
+	}
+	fmt.Printf("You roll %d dice and %v\n", num, result)
+
+}
