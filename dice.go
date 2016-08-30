547a845f4a7876a865c602036482dedc66410c3b
Yu-Han Lin <guesslin1986@gmail.com>
2016-08-31 07:51:38 +0800

    Update: 改成單獨產生單一數字，避免各數字僅出現一次的狀況

--- dice.go
+++ dice.go
@@ -8,11 +8,11 @@ import (
 )
 
 // pickRandom generate []int for 1~n, and take m elements from it
-func pickRandom(n, m int) []int {
-	res := make([]int, m)
+func pickRandom(n int) int {
+	var res int
 	for i := 1; i <= n; i++ {
-		if j := rand.Intn(i); j < m {
-			res[i%m] = i
+		if rand.Intn(i) == 0 {
+			res = i
 		}
 	}
 	return res
@@ -24,6 +24,7 @@ func init() {
 
 func main() {
 	var num, dice int
+	var result []int
 	if len(os.Args) < 2 {
 		num = 1
 		dice = 6
@@ -31,7 +32,10 @@ func main() {
 		args := os.Args[1]
 		fmt.Sscanf(args, "%dd%d", &num, &dice)
 	}
-	result := pickRandom(dice, num)
+	for i := 1; i <= num; i++ {
+		result = append(result, pickRandom(dice))
+	}
+	// result := pickRandom(dice, num)
 	fmt.Printf("You roll %d dice and %v\n", num, result)
 
 }
